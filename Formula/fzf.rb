707732a8ed32a9fc39991bc9392d4d58eefef8df
swimminghao1 <18709211491@163.com>
2025-08-25 20:56:33 +0800

    字符串处理

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -1,26 +1,26 @@
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.30.0.tar.gz"
-  sha256 "a3428f510b7136e39104a002f19b2e563090496cb5205fa2e4c5967d34a20124"
+  url "https://github.com/junegunn/fzf/archive/0.25.0.tar.gz"
+  sha256 "ccbe13733d692dbc4f0e4c0d40c053cba8d22f309955803692569fb129e42eb0"
   license "MIT"
-  head "https://github.com/junegunn/fzf.git", branch: "master"
+  head "https://github.com/junegunn/fzf.git"
 
 #  bottle do
-#    sha256 cellar: :any_skip_relocation, arm64_monterey: "c776176e54740ed75d522099a7f20fd5ad9cc6043980ee42f513b8d776899b30"
-#    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c776176e54740ed75d522099a7f20fd5ad9cc6043980ee42f513b8d776899b30"
-#    sha256 cellar: :any_skip_relocation, monterey:       "dadc361593396ced96c13d3695019a302d124e6cfe8ab6ffe65ba7877025a706"
-#    sha256 cellar: :any_skip_relocation, big_sur:        "dadc361593396ced96c13d3695019a302d124e6cfe8ab6ffe65ba7877025a706"
-#    sha256 cellar: :any_skip_relocation, catalina:       "dadc361593396ced96c13d3695019a302d124e6cfe8ab6ffe65ba7877025a706"
-#    sha256 cellar: :any_skip_relocation, x86_64_linux:   "528898433166413e8b928311b25b05b172775eef02d299f55c4fa5ab8282170e"
+#    cellar :any_skip_relocation
+#    sha256 "e2dbde85df14e4381a1ca63257a78cd3cbc60bd7c2f44910a738ad106d6517cf" => :big_sur
+#    sha256 "91df5b8ae8cb1dae31790538376415c4a89af75207e56305d1f2f8fe8b09729d" => :arm64_big_sur
+#    sha256 "20bae77a697278028e2a363c0286ac18d4e9f140cabf2b08ec4e3409dcde56d4" => :catalina
+#    sha256 "5af94f0a2a6f33b165b509a9f02cd7862fcd209c066b1e888c012e18ffb65e43" => :mojave
 #  end
 
-  depends_on "go"
+#  depends_on "go" => :build
 
   uses_from_macos "ncurses"
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version} -X main.revision=brew")
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    system "go", "build", "-o", bin/"fzf", "-ldflags", "-X main.version=#{version} -X main.revision=brew"
 
     prefix.install "install", "uninstall"
     (prefix/"shell").install %w[bash zsh fish].map { |s| "shell/key-bindings.#{s}" }
@@ -42,6 +42,6 @@ class Fzf < Formula
 
   test do
     (testpath/"list").write %w[hello world].join($INPUT_RECORD_SEPARATOR)
-    assert_equal "world", pipe_output("#{bin}/fzf -f wld", (testpath/"list").read).chomp
+    assert_equal "world", shell_output("cat #{testpath}/list | #{bin}/fzf -f wld").chomp
   end
 end
