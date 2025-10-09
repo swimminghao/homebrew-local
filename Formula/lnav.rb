672dd10d74d174cc491fb332aceccc65ca98e877
swimminghao <18709211491@163.com>
2025-10-09 20:47:09 +0800

    dbever crack

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -1,8 +1,8 @@
 class Lnav < Formula
   desc "Curses-based tool for viewing and analyzing log files"
   homepage "https://lnav.org/"
-  url "https://github.com/tstack/lnav/releases/download/v0.12.4/lnav-0.12.4.tar.gz"
-  sha256 "e1e70c9e5a2fce21da80eec9b9c3adb09fd05e03986285098a9f2567c1eb4792"
+  url "https://github.com/tstack/lnav/releases/download/v0.9.0/lnav-0.9.0.tar.gz"
+  sha256 "03e15449a87fa511cd19c6bb5e95de4fffe17612520ff7683f2528d3b2a7238f"
   license "BSD-2-Clause"
 
   livecheck do
@@ -12,38 +12,27 @@ class Lnav < Formula
 
 
   head do
-    url "https://github.com/tstack/lnav.git", branch: "master"
+    url "https://github.com/tstack/lnav.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "re2c" => :build
   end
 
-  depends_on "rust" => :build
-  depends_on "libarchive"
-  depends_on "libunistring"
-  depends_on "ncurses"
-  depends_on "pcre2"
+  depends_on "pcre"
   depends_on "readline"
   depends_on "sqlite"
 
-  uses_from_macos "bzip2"
-  uses_from_macos "curl"
-  uses_from_macos "zlib"
-
   def install
     system "./autogen.sh" if build.head?
-    system "./configure", "--with-sqlite3=#{Formula["sqlite"].opt_prefix}",
-                          "--with-readline=#{Formula["readline"].opt_prefix}",
-                          "--with-libarchive=#{Formula["libarchive"].opt_prefix}",
-                          "--with-ncurses=#{Formula["ncurses"].opt_prefix}",
-                          *std_configure_args
-    system "make", "install", "V=1"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-sqlite=#{Formula["sqlite"].opt_prefix}",
+                          "--with-readline=#{Formula["readline"].opt_prefix}"
+    system "make", "install"
   end
 
   test do
-    system bin/"lnav", "-V"
-
-    assert_match "col1", pipe_output("#{bin}/lnav -n -c ';from [{ col1=1 }] | take 1'", "foo")
+    system "#{bin}/lnav", "-V"
   end
 end
