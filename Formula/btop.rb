c1e68291282dff4a60d298851ce848b6ca16f18e
swimminghao1 <18709211491@163.com>
2026-01-26 11:41:40 +0800

    navidrome 客户端

--- Formula/btop.rb
+++ Formula/btop.rb
@@ -1,22 +1,38 @@
 class Btop < Formula
   desc "Resource monitor. C++ version and continuation of bashtop and bpytop"
   homepage "https://github.com/aristocratos/btop"
-  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.2.5.tar.gz"
-  sha256 "93ff23276f2c626282037766542543dad5964fb6117c7902c4a4899607f8c95f"
+  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "331d18488b1dc7f06cfa12cff909230816a24c57790ba3e8224b117e3f0ae03e"
   license "Apache-2.0"
   head "https://github.com/aristocratos/btop.git", branch: "main"
 
 
-  depends_on "coreutils" => :build
-#  depends_on "gcc"
+  on_macos do
+    depends_on "coreutils" => :build
+#    depends_on "gcc" if DevelopmentTools.clang_build_version <= 1403
+  end
 
-  fails_with :clang # -ftree-loop-vectorize -flto=12 -s
-  # GCC 10 at least is required
-  fails_with gcc: "5"
-  fails_with gcc: "6"
-  fails_with gcc: "7"
-  fails_with gcc: "8"
-  fails_with gcc: "9"
+  on_ventura do
+    depends_on "gcc"
+    fails_with :clang
+  end
+
+  on_arm do
+    depends_on "gcc"
+    depends_on macos: :ventura
+    fails_with :clang
+  end
+
+  # -ftree-loop-vectorize -flto=12 -s
+  fails_with :clang do
+    build 1403
+    cause "Requires C++20 support"
+  end
+
+  fails_with :gcc do
+    version "9"
+    cause "requires GCC 10+"
+  end
 
   def install
     ENV["CXX"] = "#{Formula["gcc"].opt_bin}/g++-14"
@@ -25,22 +41,26 @@ class Btop < Formula
   end
 
   test do
-    config = (testpath/".config/btop")
-    mkdir config/"themes"
-    (config/"btop.conf").write <<~EOS
-      #? Config file for btop v. #{version}
-
-      update_ms=2000
-      log_level=DEBUG
-    EOS
-
     require "pty"
     require "io/console"
 
-    r, w, pid = PTY.spawn("#{bin}/btop")
-    r.winsize = [80, 130]
-    sleep 5
-    w.write "q"
+    config = (testpath/".config/btop")
+    mkdir config/"themes"
+    begin
+      (config/"btop.conf").write <<~EOS
+        #? Config file for btop v. #{version}
+
+        update_ms=2000
+        log_level=DEBUG
+      EOS
+
+      r, w, pid = PTY.spawn("#{bin}/btop")
+      r.winsize = [80, 130]
+      sleep 5
+      w.write "q"
+    rescue Errno::EIO
+      # Apple silicon raises EIO
+    end
 
     log = (config/"btop.log").read
     assert_match "===> btop++ v.#{version}", log
