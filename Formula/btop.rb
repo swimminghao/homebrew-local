074e277cc7a51da4f242236816c20b4af04da5f9
swimminghao1 <18709211491@163.com>
2026-01-26 22:27:25 +0800

    navidrome 客户端

--- Formula/btop.rb
+++ Formula/btop.rb
@@ -36,7 +36,7 @@ class Btop < Formula
 
   def install
     #system "make", "CXX=/usr/local/Cellar/gcc/14.1.0_1/bin/g++-14", "STRIP=true"
-    system "make", "CXX=/usr/local/Cellar/gcc@14/14.3.0/bin/g++-14 -v", "STRIP=true"
+    system "make", "CXX=/usr/local/Cellar/gcc@14/14.3.0/bin/g++-14", "STRIP=true"
 
     system "make", "PREFIX=#{prefix}", "install"
   end
