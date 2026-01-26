72d5691cad824510235b534a59a095ab09213a58
swimminghao1 <18709211491@163.com>
2026-01-26 20:32:42 +0800

    navidrome 客户端

--- Formula/btop.rb
+++ Formula/btop.rb
@@ -1,40 +1,52 @@
 class Btop < Formula
-  desc "Resource monitor for macOS Catalina"
+  desc "Resource monitor. C++ version and continuation of bashtop and bpytop"
   homepage "https://github.com/aristocratos/btop"
   url "https://github.com/aristocratos/btop/archive/refs/tags/v1.2.13.tar.gz"
-  sha256 "f3f6f2a5d7c6b5a5d5f5d5d5f5d5d5f5d5d5f5d5d5f5d5d5f5d5d5f5d5d5f5d5d5f"
+  sha256 "668dc4782432564c35ad0d32748f972248cc5c5448c9009faeb3445282920e02"
   license "Apache-2.0"
+  head "https://github.com/aristocratos/btop.git", branch: "main"
 
-  depends_on "gcc" => :build
+
+  on_macos do
+    depends_on "coreutils" => :build
+    depends_on "gcc"
+  end
+
+  fails_with :clang # -ftree-loop-vectorize -flto=12 -s
+
+  fails_with :gcc do
+    version "9"
+    cause "requires GCC 10+"
+  end
 
   def install
-    # 直接使用 g++-14
-    cxx = "/usr/local/bin/g++-14"
-    
-    unless File.exist?(cxx)
-      # 如果 g++-14 不在 /usr/local/bin，在其他地方查找
-      if File.exist?("#{Formula["gcc"].opt_bin}/g++-14")
-        cxx = "#{Formula["gcc"].opt_bin}/g++-14"
-      else
-        # 查找其他版本
-        gcc_versions = Dir["/usr/local/bin/g++-*"].sort
-        cxx = gcc_versions.last if gcc_versions.any?
-      end
-    end
-    
-    ohai "使用编译器: #{cxx}"
-    
-    # 修改 Makefile
-    inreplace "Makefile", "-std=c++23", "-std=c++20"
-    inreplace "Makefile", "-flto=$(THREADS)", ""
-    inreplace "Makefile", "-ftree-loop-vectorize", ""
-    
-    # 编译
-    system "make", "CXX=#{cxx}", "STRIP=true"
+    #system "make", "CXX=#{ENV.cxx}", "STRIP=true"
+    system "make", "CXX=/usr/local/Cellar/gcc@14/14.3.0/bin/g++-14 -v", "STRIP=true"
     system "make", "PREFIX=#{prefix}", "install"
   end
-  
+
   test do
-    assert_match "btop", shell_output("#{bin}/btop --version 2>&1", 1)
+    config = (testpath/".config/btop")
+    mkdir config/"themes"
+    (config/"btop.conf").write <<~EOS
+      #? Config file for btop v. #{version}
+
+      update_ms=2000
+      log_level=DEBUG
+    EOS
+
+    require "pty"
+    require "io/console"
+
+    r, w, pid = PTY.spawn("#{bin}/btop")
+    r.winsize = [80, 130]
+    sleep 5
+    w.write "q"
+
+    log = (config/"btop.log").read
+    assert_match "===> btop++ v.#{version}", log
+    refute_match(/ERROR:/, log)
+  ensure
+    Process.kill("TERM", pid)
   end
 end
