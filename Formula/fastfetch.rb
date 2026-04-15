c54aa63ce52b1cb0846263e20802f13bf3dd9923
swimminghao1 <18709211491@163.com>
2026-04-15 10:57:11 +0800

    fastfetch

---
+++ Formula/fastfetch.rb
@@ -0,0 +1,28 @@
+class Fastfetch < Formula
+  desc "Like neofetch, but much faster because written in C"
+  homepage "https://github.com/LinusDierheimer/fastfetch"
+  url "https://github.com/LinusDierheimer/fastfetch/archive/refs/tags/1.8.1.tar.gz"
+  sha256 "5db8ef1b4eba470b55c905ab07e2188bb2e3b4bac7aa868284e0da58c3a36926"
+  license "MIT"
+  head "https://github.com/LinusDierheimer/fastfetch.git", branch: "dev"
+
+  depends_on "chafa" => :build
+  depends_on "cmake" => :build
+  depends_on "glib" => :build
+  depends_on "imagemagick" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vulkan-loader" => :build
+
+  uses_from_macos "zlib" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "fastfetch", shell_output("#{bin}/fastfetch --version")
+    assert_match "OS", shell_output("#{bin}/fastfetch --structure OS --logo none --hide-cursor false")
+  end
+end
